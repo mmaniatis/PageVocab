aea0fe3f13e30b7b8e53ea23bb18c53bc1c1e3af
Mikejohnmaniatis <mikejohnmaniatis@gmail.com>
2021-01-04 19:58:28 -0600

    Retrieving user DOM. Will be changing next. Read Users DOM complete.

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-        translate();
-        chrome.tabs.query({ active: true }, function(tabs) {
-            const msg = "Hello from background 🔥";
-            console.log(msg);
-        })
+        getDomInfo();
     }
-  });
+});
 function translate() {
     var url = "https://translation.googleapis.com/language/translate/v2";
     //Strings requiring translation
@@ -20,5 +16,15 @@ function translate() {
         .then(data => console.log(data.data.translations[0].translatedText));
         // .then(replaceVocabWord());
 }
-//TODO: Need to access current tabs DOM and log in console.
-// UPDATE 1/4/2021 - Successfully talking between content / background scripts. Now I can send dom, add methods, and print that.
+
+function getDomInfo() {
+
+        chrome.tabs.query({ active:true, currentWindow:true} , function(tab){
+            console.log('Sending..');
+            chrome.tabs.sendMessage(tab[0].id, { "message": 'test' }, processDom); 
+        });
+}
+
+function processDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 // Listening to messages in Context Script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
-    // Callback
-    sendResponse({ message: 'Content script has received that message ⚡' })
+    // console.log (document.body.innerHTML);
+    console.log('OnMessage hit');
+    sendResponse(document.querySelectorAll(["p", "h1", "h2", "h3", "h4", "h5"]));
 })
