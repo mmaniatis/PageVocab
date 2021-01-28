e90333b7f12ea099cd9b5f4ee8bebff750620ccc
Mikejohnmaniatis <mikejohnmaniatis@gmail.com>
2021-01-27 21:55:46 -0600

    Starting to OOO refactor.

--- main/js/background.js
+++ main/js/background.js
@@ -1,7 +1,32 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
+class Translator {
+
+    constructor () {
+    }
+
+    async translate(word, target) {
+        console.log('Word to translate: ' + word);
+        console.log('Target language: ' + target);
+        var url = "https://translation.googleapis.com/language/translate/v2";
+        url += "?q=" + word; 
+        url += "&target=" + target;
+        url += "&key=AIzaSyBPBL9scxJGe0GOjx2Ougtb2MWbdzw_bP4";
+        const response = await fetch(url);
+        return response;
+    }
+    
+    translatePage() {
+        chrome.tabs.query({ currentWindow:true, active:true} , function(tab){
+            console.log('Sending to content script for translation...');
+            chrome.tabs.sendMessage(tab[0].id, { action: "translatePage" }); 
+        });
+    }
+}
+const translator = new Translator();
+
+chrome.webNavigation.onCompleted.addListener(function(details) {    
     if (!(details.url.includes('google')) ) {
         if(details.frameId==0){
-            translatePage();
+            translator.translatePage();
         }
     }
 });
@@ -12,66 +37,23 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (result.targetLanguage != null) {
             target = result.targetLanguage;
         }
-        translate(request.wordToTranslate, target).then(res => res.json()).then(json => 
+        translator.translate(request.wordToTranslate, target).then(res => res.json()).then(json => 
             {
                 const obj = json.data.translations[0].translatedText;
                 sendResponse(obj);
             });
     }); 
-    
-
     return true;
 });
 
-async function translate(word, target) {
-    // console.log('Word to translate: ' + word);
-    // console.log('Target language: ' + target);
-    var url = "https://translation.googleapis.com/language/translate/v2";
-    url += "?q=" + word; 
-    url += "&target=" + target;
-    url += "&key=AIzaSyBPBL9scxJGe0GOjx2Ougtb2MWbdzw_bP4";
-    const response = await fetch(url);
-    return response;
-}
-
-async function getTargetLanguage() {
-    return chrome.storage.sync.get(['targetLanguage'], function(result) {
-        var target = 'es';
-        if (result.targetLanguage != null) {
-            target = result.targetLanguage;
-        }
-        return target;
-    });
-}
-
-function translatePage() {
-    chrome.tabs.query({ currentWindow:true, active:true} , function(tab){
-        console.log('Sending to content script for translation...');
-        chrome.tabs.sendMessage(tab[0].id, { action: "translatePage" }); 
-    });
-}
-
-async function getLocaleList() {
-    const data = {"target": "en"}
-    var url = "https://translation.googleapis.com/language/translate/v2/languages";
-     url += "?key=AIzaSyBPBL9scxJGe0GOjx2Ougtb2MWbdzw_bP4";
-    const response = await fetch(url, {
-        method: "GET"
-    }).then(res => res.json()).then(json => {console.log(json)});
-    return response;
-}
 
 
-
-// function populateLocaleList() {
-//     var localeList = document.getElementById('localeList');
-//     var supportedLanguages = {"Spanish":"es"};
-
-//     for(var key in supportedLanguages) {
-//         var localeListItem = document.createElement("li");
-//         localeListItem.innerHTML = key;
-//         localeListItem.value = supportedLanguages[key];
-//         localeList.append(localeListItem);
-//         // localeListItem.onclick = "changeTarget()";
-//     }
+// async function getLocaleList() {
+//     const data = {"target": "en"}
+//     var url = "https://translation.googleapis.com/language/translate/v2/languages";
+//      url += "?key=AIzaSyBPBL9scxJGe0GOjx2Ougtb2MWbdzw_bP4";
+//     const response = await fetch(url, {
+//         method: "GET"
+//     }).then(res => res.json()).then(json => {console.log(json)});
+//     return response;
 // }
