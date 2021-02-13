7b2eb4fa610315b751b582406620afce085130e8
Michael Maniatis <michaelmaniatis@Michaels-MacBook-Pro.local>
2021-02-12 19:20:50 -0600

    Refactored the code in content.js some more.

--- main/js/content.js
+++ main/js/content.js
@@ -2,14 +2,11 @@ class DomInteractor {
     translations_performed = 0;
     node_array = []
     text_array = [];
-    dom;
     original_word;
-    constructor() {
-        this.original_word = "";
-    }
+    constructor(){}
 
     init() {
-        this.build_p_array(this.dom, this.text_array);
+        this.build_p_array($('*'), this.text_array.sort(function(a,b) { return (a.length < b.length ? 1 : -1)}));
         this.original_word = this.selectRandomWord(3);
     }
     
@@ -18,14 +15,13 @@ class DomInteractor {
     */
     selectRandomWord(wordLength) {
         if(this.text_array.length > 0) {
-            this.text_array.sort(function(a,b) { return (a.length < b.length ? 1 : -1)});
-            let text_to_choose_random_word_from = this.text_array[getRandomInt(Math.floor(this.text_array.length/2))]            
-            .replace(/[^a-zA-Z\s]/g, "")
-            .split(" ")
-            .filter(x => x.length > wordLength);
+            let text_to_choose_random_word_from = this.text_array
+                [getRandomInt(Math.floor(this.text_array.length/2))]            
+                .replace(/[^a-zA-Z\s]/g, "")
+                .split(" ")
+                .filter(x => x.length > wordLength);
             return text_to_choose_random_word_from[getRandomInt(text_to_choose_random_word_from.length-1)];    
         }
-        
     }
 
     build_p_array(node, text_array)
@@ -41,8 +37,7 @@ class DomInteractor {
 
     build_array_helper(node, text_array) {
         node.each(i => {
-            if(checkVisible(node[i], "visible") && node[i].firstChild != null && (node[i].tagName == 'P') && !(anchorTagFound(node[i]))) {
-                
+            if(node[i].firstChild != null && (node[i].tagName == 'P') && !(anchorTagFound(node[i]))) {
                 this.node_array.push(node[i]);
                 text_array.push(node[i].innerText);
             }
@@ -53,29 +48,28 @@ class DomInteractor {
     translateAndReplaceWord(word_to_translate) {
         var self = this;
         chrome.runtime.sendMessage(({wordToTranslate: word_to_translate}), function(translated_word) {
-            self.replaceWord($('*'), translated_word, word_to_translate);
+            self.replaceWordNew(self.node_array, translated_word, word_to_translate);
         });
     }
 
-    replaceWord(node, translated_word, word_to_translate) {
-        console.log(word_to_translate);
-        console.log(translated_word);
+    replaceWordNew(node_array, translated_word, word_to_translate) {
         const regex = new RegExp("(\\b"+word_to_translate+"\\b|\\b" +capitalize(word_to_translate)+"\\b"+lower(word_to_translate)+"\\b)(?![^<]*?<\/a>)(?!([^<]+)?>)", 'g');
-        $.each(this.node_array, function() {
-            $(this).html($(this).html().replace(regex, (regex,'<mark class="page-vocab-tooltip" id="pageVocabToolTip">' + translated_word + '<span class="page-vocab-tooltiptext">'+ word_to_translate +'</span> </mark>')));
-        })
+        for(var node in node_array) {
+            var currentElem = $(node_array[node]);
+            currentElem
+                .html(currentElem
+                        .html()
+                        .replace(regex, (regex,'<mark class="page-vocab-tooltip" id="pageVocabToolTip">' + translated_word + '<span class="page-vocab-tooltiptext">'+ word_to_translate +'</span> </mark>')));
+
+        }   
     }
 
     undoTranslation(original_word) {
         var translated_nodes = document.getElementsByClassName('page-vocab-tooltip');
         var count = translated_nodes.length-1;
         $.each(translated_nodes, function (i) {
-            const new_node = document.createElement('s');
-            // translated_nodes[i].outerHTML = '<span>' + original_word + '<span>';
             translated_nodes[count].outerHTML = '<span>' + original_word + '<span>';
-            // $('#'+translated_nodes[count].id).replaceWith('<span>' + original_word + '<span>');
             count--;
-
         });
 
         this.beginTranslate()
@@ -102,7 +96,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             domInteractor.undoTranslation(domInteractor.original_word);
         }
         else {
-            domInteractor.dom = $('*'); 
             domInteractor.init();
             domInteractor.beginTranslate();
         }
@@ -139,16 +132,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function checkVisible( elm, evalType ) {
-    evalType = evalType || "visible";
-
-    var vpH = $(window).height(), // Viewport Height
-        st = $(window).scrollTop(), // Scroll Top
-        y = $(elm).offset().top,
-        elementHeight = $(elm).height();
-
-    if (evalType === "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
-}
 
 function anchorTagFound(elm) {
     var anchor = false;
